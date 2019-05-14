fc31b19471d3d7a9e6608ab8a7eb9f3c6ccf574e
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-05-14 12:26:10 +0100

    add event for body document

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const body = document.body;
+
+function checkCookies() {
+    var text = "";
+  
+    if (navigator.cookieEnabled == true) {
+      text = "Welcome, Cookies are automatically enabled.";
+    } else {
+       text = "Cookies are not enabled.";
+    }
+    alert(text)
+  }
+body.addEventListener('load', checkCookies())
